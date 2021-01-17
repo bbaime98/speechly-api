fc8a909ef2642ff8f4a54fc30b6df49c0e05ee05
bien aimé <bybienaime@gmail.com>
2021-01-17 15:06:24 +0200

    add scroll to the top when recording
    - add scroll to the top when recording on  mobile version,
    - use speechSate,useRef, and useEffect when to keep track of speechstate
      changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useEffect, useRef } from 'react';
 import { Grid } from '@material-ui/core';
 import { Details, Main } from './components';
 import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
-
+import { SpeechState, useSpeechContext } from "@speechly/react-client";
 import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
- 
+  const { speechState } = useSpeechContext();
+  const main = useRef(null)
+
+  const executeScroll = () => main.current.scrollIntoView()    
+
+  useEffect(() => {
+    if (speechState === SpeechState.Recording) {
+      executeScroll();
+    }
+  }, [speechState]);
+
   return (
     <div>
       <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
         <Grid item xs={12} sm={4} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={3} className={classes.main}>
+        <Grid  ref={main} item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
         <Grid item xs={12} sm={4} className={classes.desktop}>
